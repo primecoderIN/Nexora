95d57f43e2f2535ada8bc03e8e7e3845d31c0d0b
Sanjeev Kumar <sanjeev2856@gmail.com>
2026-08-22 12:59:32 +0530

    refactor: extract Identity services configuration to IdentityServiceExtensions

--- backend/Nexora.Api/Extensions/ApiServiceExtensions.cs
+++ backend/Nexora.Api/Extensions/ApiServiceExtensions.cs
@@ -1,8 +1,4 @@
 using FluentValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Nexora.Shared.Interfaces;
-using Nexora.Modules.Identity.API.Services;
 
 namespace Nexora.Api.Extensions;
 
@@ -26,31 +22,6 @@ public static class ApiServiceExtensions
         // Register FluentValidation
         services.AddValidatorsFromAssembly(typeof(Nexora.Modules.Identity.Application.Tenants.CreateTenant.CreateTenantCommandValidator).Assembly);
         
-        return services;
-    }
-
-    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddHttpContextAccessor();
-        services.AddScoped<ICurrentUserContext, CurrentUserContext>();
-
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                // These settings point to our local Keycloak docker container
-                options.Authority = configuration["Authentication:Keycloak:Authority"] ?? "http://localhost:8080/realms/nexora";
-                options.RequireHttpsMetadata = false; // Because we are running locally over HTTP
-                options.Audience = configuration["Authentication:Keycloak:Audience"] ?? "account";
-                
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true
-                };
-            });
-
         return services;
     }
 }
---
+++ backend/Nexora.Api/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Nexora.Shared.Interfaces;
+using Nexora.Modules.Identity.API.Services;
+
+namespace Nexora.Api.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<ICurrentUserContext, CurrentUserContext>();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                // These settings point to our local Keycloak docker container
+                options.Authority = configuration["Authentication:Keycloak:Authority"] ?? "http://localhost:8080/realms/nexora";
+                options.RequireHttpsMetadata = false; // Because we are running locally over HTTP
+                options.Audience = configuration["Authentication:Keycloak:Audience"] ?? "account";
+                
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+        return services;
+    }
+}
